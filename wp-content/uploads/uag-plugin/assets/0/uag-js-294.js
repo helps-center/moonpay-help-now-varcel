fa67180700c2b4fdc5807e5d479a3409fe3c2d91
helps-center <ajeetkumarshukla820@gmail.com>
2025-07-29 02:27:24 +0530

    Add files via upload

---
+++ wp-content/uploads/uag-plugin/assets/0/uag-js-294.js
@@ -0,0 +1,13 @@
+document.addEventListener("DOMContentLoaded", function(){ window.addEventListener( 'load', function() {
+	UAGBCounter.init( '.uagb-block-c576f5d2', {"layout":"number","heading":"Years of Experience","numberPrefix":"","numberSuffix":"+","startNumber":0,"endNumber":20,"totalNumber":100,"decimalPlaces":0,"animationDuration":1500,"thousandSeparator":",","circleSize":230,"circleStokeSize":8,"isFrontend":true} );
+});
+window.addEventListener( 'load', function() {
+	UAGBCounter.init( '.uagb-block-71e8cb14', {"layout":"number","heading":"Happy Customers","numberPrefix":"","numberSuffix":"+","startNumber":0,"endNumber":700,"totalNumber":700,"decimalPlaces":0,"animationDuration":1500,"thousandSeparator":",","circleSize":230,"circleStokeSize":8,"isFrontend":true} );
+});
+window.addEventListener( 'load', function() {
+	UAGBCounter.init( '.uagb-block-ea6fab80', {"layout":"number","heading":"Award Winning","numberPrefix":"","numberSuffix":"+","startNumber":0,"endNumber":8,"totalNumber":100,"decimalPlaces":0,"animationDuration":1500,"thousandSeparator":",","circleSize":230,"circleStokeSize":8,"isFrontend":true} );
+});
+window.addEventListener( 'load', function() {
+	UAGBCounter.init( '.uagb-block-be547797', {"layout":"number","heading":"Customer Satisfaction","numberPrefix":"","numberSuffix":"%","startNumber":0,"endNumber":90,"totalNumber":100,"decimalPlaces":0,"animationDuration":1500,"thousandSeparator":",","circleSize":230,"circleStokeSize":8,"isFrontend":true} );
+});
+ });
